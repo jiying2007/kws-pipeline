366941306f7915a4537a64a639d8292594867fef
jiying2007 <33591504+jiying2007@users.noreply.github.com>
2026-08-28 22:22:43 +0800

    feat: add fixed-state streaming frontend

---
+++ src/frontend.h
@@ -0,0 +1,17 @@
+#ifndef KWS_PIPELINE_FRONTEND_H
+#define KWS_PIPELINE_FRONTEND_H
+#include <stdint.h>
+#include "kws_pipeline/kws.h"
+typedef struct kws_frontend {
+  uint32_t frame_len, hop, fill;
+  uint16_t feature_dim;
+  int16_t pcm[512];
+  float window[512], re[512], im[512];
+  uint16_t mel_bins[KWS_MAX_FEATURE_DIM + 2u];
+  float last_dbfs;
+} kws_frontend_t;
+void kws_frontend_init(kws_frontend_t *fe, const kws_model_t *model);
+void kws_frontend_reset(kws_frontend_t *fe);
+int kws_frontend_push(kws_frontend_t *fe, int16_t sample, float *out_features);
+float kws_frontend_last_dbfs(const kws_frontend_t *fe);
+#endif
